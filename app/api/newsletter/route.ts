c361f72a95c1a70416166eb34d4bd6797695d7f1
ajitesh123 <ajiteshleo@gmail.com>
2024-11-17 11:07:20 +0530

    added newletter

--- app/api/newsletter/route.ts
+++ app/api/newsletter/route.ts
@@ -1,9 +1,63 @@
-import { NewsletterAPI } from 'pliny/newsletter'
-import siteMetadata from '@/data/siteMetadata'
+import { NextResponse } from 'next/server'
 
-const handler = NewsletterAPI({
-  // @ts-ignore
-  provider: siteMetadata.newsletter.provider,
-})
+export async function POST(req: Request) {
+  const { email } = await req.json()
 
-export { handler as GET, handler as POST }
+  if (!email || !email.length) {
+    return NextResponse.json(
+      {
+        error: 'Email is required',
+      },
+      {
+        status: 400,
+      }
+    )
+  }
+
+  try {
+    const API_KEY = process.env.BEEHIVE_API_KEY
+    const PUBLICATION_ID = process.env.BEEHIVE_PUBLICATION_ID
+
+    const response = await fetch(
+      `https://api.beehiiv.com/v2/publications/${PUBLICATION_ID}/subscriptions`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${API_KEY}`,
+        },
+        body: JSON.stringify({
+          email: email,
+          reactivate_existing: false,
+          send_welcome_email: true,
+        }),
+      }
+    )
+
+    if (!response.ok) {
+      const error = await response.json()
+      return NextResponse.json(
+        {
+          error: error.message || 'Error subscribing to newsletter',
+        },
+        {
+          status: response.status,
+        }
+      )
+    }
+
+    return NextResponse.json(
+      { message: 'Successfully subscribed to the newsletter' },
+      { status: 201 }
+    )
+  } catch (error) {
+    return NextResponse.json(
+      {
+        error: 'Error subscribing to the newsletter',
+      },
+      {
+        status: 500,
+      }
+    )
+  }
+}
